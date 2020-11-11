ec3a88e378053da3a3586ad047bb86901f66170b
Mingsicogi <codedoctor119@gmail.com>
2020-11-12 02:16:59 +0900

    + Add Entity structure

---
+++ src/main/java/my/study/redis/sample/list/usecase/twitter/entity/PostsLike.java
@@ -0,0 +1,20 @@
+package my.study.redis.sample.list.usecase.twitter.entity;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "posts_like")
+public class PostsLike {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "posts_like_id")
+    private long id;
+
+    @Column(name = "reg_ymdt")
+    private LocalDateTime regYmdt;
+}
