6dd00e9ad0a93a9198796cd619fd29fbdb77ed54
Mingsicogi <codedoctor119@gmail.com>
2020-11-13 04:45:56 +0900

    + Add Twitter example

--- src/main/java/my/study/redis/sample/list/usecase/twitter/TwitterController.java
+++ src/main/java/my/study/redis/sample/list/usecase/twitter/TwitterController.java
@@ -6,6 +6,7 @@ import my.study.redis.sample.list.usecase.twitter.entity.Posts;
 import my.study.redis.sample.list.usecase.twitter.entity.Users;
 import my.study.redis.sample.list.usecase.twitter.repository.PostsRepository;
 import my.study.redis.sample.list.usecase.twitter.repository.UserRepository;
+import my.study.redis.sample.string.simple.StringRedisSampleService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,6 +21,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+import static java.util.stream.Collectors.reducing;
 import static java.util.stream.Collectors.toList;
 
 @Controller
@@ -28,11 +30,14 @@ import static java.util.stream.Collectors.toList;
 public class TwitterController {
 
     private final ListRedisSampleService listRedisSampleService;
+    private final StringRedisSampleService stringRedisSampleService;
 
     private final PostsRepository postsRepository;
     private final UserRepository userRepository;
 
     static final String TWITTER_LATEST_POST_COMMON_KEY = "twitter:post:latest:";
+    static final String TWITTER_POST_TOTAL_COUNT_KEY = "twitter:post:total:count";
+    static final Integer LATEST_COUNT = 100;
 
     @GetMapping("/posts/latest10")
     public String list(@RequestParam String userId, Model model) {
@@ -40,10 +45,11 @@ public class TwitterController {
 
         stopWatch.start();
         Users user = userRepository.findByUserId(userId);
-        List<Posts> latestPostsList = postsRepository.findTop10ByUserOrderByRegYmdtDescIdDesc(user);
+        List<Posts> latestPostsList = postsRepository.findTop100ByUserOrderByRegYmdtDescIdDesc(user);
         stopWatch.stop();
 
         model.addAttribute("latestPostsList", latestPostsList);
+        model.addAttribute("totalPostsCount", getAllPostCount());
         model.addAttribute("user", user);
         model.addAttribute("pageLoadingTime", stopWatch.getTotalTimeSeconds());
         model.addAttribute("useRedis", false);
@@ -58,32 +64,43 @@ public class TwitterController {
         stopWatch.start();
 
         String redisKey = TWITTER_LATEST_POST_COMMON_KEY + userId;
-        Optional<List<byte[]>> redisData = listRedisSampleService.getListStartToEnd(redisKey.getBytes(), 0, -1);
+        Optional<List<byte[]>> redisData = listRedisSampleService.getListStartToEnd(redisKey.getBytes(), 0, LATEST_COUNT - 1);
         if(redisData.isPresent()) {
-            if(redisData.get().size() == 10) {
+            if(redisData.get().size() == LATEST_COUNT) {
                 model.addAttribute("latestPostsList", redisData.get().stream().map(SerializationUtils::deserialize).collect(toList()));
             } else {
-                getLatestPostAndLoadPkListOnRedis(model, userId, redisKey);
+                getLatestPostListOnRedis(model, userId, redisKey);
             }
         } else {
-            getLatestPostAndLoadPkListOnRedis(model, userId, redisKey);
+            getLatestPostListOnRedis(model, userId, redisKey);
         }
         stopWatch.stop();
 
         model.addAttribute("user", userRepository.findByUserId(userId));
+        model.addAttribute("totalPostsCount", getAllPostCount());
         model.addAttribute("pageLoadingTime", stopWatch.getTotalTimeSeconds());
         model.addAttribute("useRedis", true);
 
         return "twitter/list";
     }
 
-    private void getLatestPostAndLoadPkListOnRedis(Model model, String userId, String redisKey) {
+    private void getLatestPostListOnRedis(Model model, String userId, String redisKey) {
         Users user = userRepository.findByUserId(userId);
-        List<Posts> latestPostList = postsRepository.findTop10ByUserOrderByRegYmdtDescIdDesc(user);
+        List<Posts> latestPostList = postsRepository.findTop100ByUserOrderByRegYmdtDescIdDesc(user);
         listRedisSampleService.pushToRight(redisKey.getBytes(), latestPostList.stream().map(SerializationUtils::serialize).collect(toList()).toArray(byte[][]::new));
-        listRedisSampleService.cutFromTo(redisKey, 0, 9);
+        listRedisSampleService.cutFromTo(redisKey, 0, LATEST_COUNT - 1);
 
         model.addAttribute("latestPostsList", latestPostList);
+    }
+
+    private String getAllPostCount() {
+        Optional<String> countAll = stringRedisSampleService.get(TWITTER_POST_TOTAL_COUNT_KEY).or(() -> {
+            Integer allCount = postsRepository.findAllCount();
+            stringRedisSampleService.save(TWITTER_POST_TOTAL_COUNT_KEY, String.valueOf(allCount));
+            return Optional.ofNullable(String.valueOf(allCount));
+        });
+
+        return countAll.get();
     }
 
     @GetMapping("/posts/add")
@@ -95,8 +112,9 @@ public class TwitterController {
 
         String redisKey = TWITTER_LATEST_POST_COMMON_KEY + userId;
         listRedisSampleService.pushToLeft(redisKey.getBytes(), SerializationUtils.serialize(savedPost));
+        stringRedisSampleService.countUp(TWITTER_POST_TOTAL_COUNT_KEY);
 
-        listRedisSampleService.cutFromTo(redisKey, 0, 9);
+        listRedisSampleService.cutFromTo(redisKey, 0, LATEST_COUNT - 1);
 
         return ResponseEntity.ok("OK");
     }
